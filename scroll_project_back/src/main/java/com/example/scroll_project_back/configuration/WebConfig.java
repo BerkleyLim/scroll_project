5317a84f62582e37766cd9adec22a4c6232ade37
berkleylim <berkleylim16@gmail.com>
2023-07-08 20:38:04 +0900

    ci: 백엔드 설정 - 현재 mybatis까지 연동 완료

--- scroll_project_back/src/main/java/com/example/scroll_project_back/configuration/WebConfig.java
+++ scroll_project_back/src/main/java/com/example/scroll_project_back/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.scroll_project_back.configration;
+package com.example.scroll_project_back.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
