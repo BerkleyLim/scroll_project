914ca3aeac422a8be10dea8431c620d3f2da8aad
berkleylim <berkleylim16@gmail.com>
2023-07-08 16:59:09 +0900

    ci: Front + Back 설치
    Front 셋팅 완료, Back 환경설정 셋팅예정

---
+++ scroll_project_front/src/main/java/com/example/scroll_project_front/ScrollProjectFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.scroll_project_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollProjectFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrollProjectFrontApplication.class, args);
+	}
+
+}
---
+++ scroll_project_front/src/test/java/com/example/scroll_project_front/ScrollProjectFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scroll_project_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrollProjectFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
